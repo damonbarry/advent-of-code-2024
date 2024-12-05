2948e117fe345837c7fc7397fc5770d9e3e2b512
Damon Barry <damonb@microsoft.com>
2024-12-05 13:27:57 -0800

    Solve day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 fn main() {
     calculate_left_right_list_distance();
+    calculate_left_right_list_similarity_score();
 }
 
 fn calculate_left_right_list_distance() {
@@ -25,5 +26,29 @@ fn calculate_left_right_list_distance() {
         .map(|(l, r)| r.abs_diff(l))
         .sum::<u64>();
 
-    println!("The total distance between left and right lists is {}", sum,);
+    println!("The total distance between left and right lists is {}", sum);
+}
+
+fn calculate_left_right_list_similarity_score() {
+    let input = fs::read_to_string("input/day1.txt").unwrap();
+    let lines = input.lines();
+    let (left, right): (Vec<_>, Vec<_>) = lines
+        .map(|l| -> (u64, u64) {
+            l.split_ascii_whitespace()
+                .map(|id| id.parse::<u64>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+
+    let right_id_counts = right.into_iter().counts();
+    let similarity_score = left
+        .into_iter()
+        .map(|id| id * *right_id_counts.get(&id).unwrap_or(&0) as u64)
+        .sum::<u64>();
+
+    println!(
+        "The similarity score of the left and right lists is {}",
+        similarity_score
+    );
 }
