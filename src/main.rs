066c144bd83cfbb9fb2f1f40eea3f6b6537a5d0d
Damon Barry <damonb@microsoft.com>
2024-12-05 12:22:29 -0800

    Solve day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use itertools::Itertools;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input/day1.txt").unwrap();
+    let lines = input.lines();
+    let (mut left, mut right): (Vec<_>, Vec<_>) = lines
+        .map(|l| -> (u64, u64) {
+            l.split_ascii_whitespace()
+                .map(|id| id.parse::<u64>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+
+    left.sort_unstable();
+    right.sort_unstable();
+    let sum = left
+        .into_iter()
+        .zip(right)
+        .map(|(l, r)| r.abs_diff(l))
+        .sum::<u64>();
+
+    println!("The total distance between left and right lists is {}", sum,);
 }
