53005f1a27dab65f309079fa0d77ad2a63434870
Damon Barry <damonb@microsoft.com>
2024-12-05 12:25:28 -0800

    Move day 1 part 1 code into its own function

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,28 @@ use itertools::Itertools;
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input/day1.txt").unwrap();
-    let lines = input.lines();
-    let (mut left, mut right): (Vec<_>, Vec<_>) = lines
-        .map(|l| -> (u64, u64) {
-            l.split_ascii_whitespace()
-                .map(|id| id.parse::<u64>().unwrap())
-                .collect_tuple()
-                .unwrap()
-        })
-        .unzip();
+  calculate_left_right_list_distance()
+}
 
-    left.sort_unstable();
-    right.sort_unstable();
-    let sum = left
-        .into_iter()
-        .zip(right)
-        .map(|(l, r)| r.abs_diff(l))
-        .sum::<u64>();
+fn calculate_left_right_list_distance() {
+  let input = fs::read_to_string("input/day1.txt").unwrap();
+  let lines = input.lines();
+  let (mut left, mut right): (Vec<_>, Vec<_>) = lines
+      .map(|l| -> (u64, u64) {
+          l.split_ascii_whitespace()
+              .map(|id| id.parse::<u64>().unwrap())
+              .collect_tuple()
+              .unwrap()
+      })
+      .unzip();
 
-    println!("The total distance between left and right lists is {}", sum,);
+  left.sort_unstable();
+  right.sort_unstable();
+  let sum = left
+      .into_iter()
+      .zip(right)
+      .map(|(l, r)| r.abs_diff(l))
+      .sum::<u64>();
+
+  println!("The total distance between left and right lists is {}", sum,);
 }
